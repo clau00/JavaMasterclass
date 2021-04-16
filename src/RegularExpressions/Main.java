685f5193fbaa15503a4123b8648bd3cd0f9dce26
claudiu <danbalint_88@yahoo.com>
2021-04-16 12:54:51 +0300

    CB: #Regular Expressions: Quantifiers and Pattern and Matcher classes

--- src/RegularExpressions/Main.java
+++ src/RegularExpressions/Main.java
@@ -1,8 +1,14 @@
 package RegularExpressions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //  Introduction, Character Classes and Boundary Matchers
+
         String string = "I am a string. Yes, I am.";
         System.out.println(string);
         String yourString = string.replaceAll("I", "You");
@@ -44,6 +50,58 @@ public class Main {
         System.out.println(alphanumeric.replaceAll("\\w", "X"));
         System.out.println(hasWhiteSpace.replaceAll("\\w", "X"));
         System.out.println(hasWhiteSpace.replaceAll("\\b", "_"));
+
+        //  Quantifiers - specify how often an element occurs in a RegExp
+
+        String newAlphanumeric = "abcDeeeF12GhhiiijabcDeeeijkl99z";
+        System.out.println(newAlphanumeric.replaceAll("^abcDeee", "YYY"));
+        System.out.println(newAlphanumeric.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(newAlphanumeric.replaceAll("abcDeee", "YYY"));
+        //  1 or more "e"
+        System.out.println(newAlphanumeric.replaceAll("^abcDe+", "YYY"));
+        //  0 or more "e"
+        System.out.println(newAlphanumeric.replaceAll("^abcDe*", "YYY"));
+        System.out.println(newAlphanumeric.replaceAll("^abcDe*", "YYY"));
+        System.out.println(newAlphanumeric.replaceAll("^abcDe{2,5}", "YYY"));
+        System.out.println(newAlphanumeric.replaceAll("h+i*j", "_Y_"));
+
+        //  Pattern amd Matcher Classes
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is another paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.</p>");
+
+        String h2Pattern = "<h2>";
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+
+        matcher.reset();
+        int count = 0;
+        while(matcher.find()){
+            count++;
+            System.out.println("Occurrence " + count + " : " + matcher.start() + " to " + matcher.end());
+        }
+
+        String h2GroupPattern = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+
+        while(groupMatcher.find()){
+            System.out.println("Occurrence: " + groupMatcher.group(1));
+        }
+
+        String h2TextGroups = "(<h2>)(.+?)(/h2)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+        while(h2TextMatcher.find()){
+            System.out.println("Occurrence: " + h2TextMatcher.group(2));
+        }
+
 
     }
 }
